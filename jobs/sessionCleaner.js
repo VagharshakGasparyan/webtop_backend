1f43668a2ed307b6fc5c4a09220cabf360efbd92
vvagh <vvagharshak@gmail.com>
2024-02-13 18:12:19 +0400

    fix session cleaner

--- jobs/sessionCleaner.js
+++ jobs/sessionCleaner.js
@@ -2,17 +2,15 @@ const cron = require("node-cron");
 const {conf} = require("../config/app_config");
 const {Op} = require("sequelize");
 const queryInterface = require('../models').sequelize.getQueryInterface();
+const {DB} = require("../components/db");
+const moment = require("moment");
 
 cron.schedule('*/5 * * * *', async () => {//running every 5 minutes
-    // console.log('running session cleaner');
-    await queryInterface.bulkDelete(
-        conf.token.table,
-        {
-            updated_at: {
-                [Op.lt]: new Date(new Date() - conf.token.maxAge)//updated_at < now - maxAge
-            }
-        },
-        {}
-    );
+    console.log('running session cleaner');
+    try {
+        await DB(conf.token.table).where("updated_at", "<", moment(new Date() - conf.token.maxAge).format('yyyy-MM-DD HH:mm:ss')).delete();
+    }catch (e) {
+        console.error(e);
+    }
     // console.log('cron running at ' + moment().format('yyyy_MM_DD-HH:mm:ss'));
 });
