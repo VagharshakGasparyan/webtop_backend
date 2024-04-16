fa1e0c72d290a7fd7c13b53c0487a8ce0d82e93f
vvagh <vvagharshak@gmail.com>
2024-04-16 17:52:28 +0400

    add validation function

--- components/validate.js
+++ components/validate.js
@@ -52,60 +52,124 @@ async function unique(table, columnName, columnValue) {
     return null;
 }
 
-class ValidateClass {
-    constructor(req, fn) {
+class VRequest {
+    constructor(req, res) {
         this._req = req;
-        this._body = req.body;
-        this._files = req.files;
-        let answer = fn(this);
-
-        return answer;
+        this._body = req.body ?? {};
+        this._files = req.files ?? {};
+        this._sequence = [];
+        this._errors = null;
+    }
+    _pushErr(key, err){
+        if(this._errors){
+            if(key in this._errors){
+                this._errors[key].push(err);
+            }else{
+                this._errors[key] = [err];
+            }
+        }else{
+            this._errors = {[key]: [err]};
+        }
+    }
+    key(reqKey){
+        this._sequence.push({'key' : reqKey});
+        return this;
     }
     number(){
+        this._sequence.push('number');
         return this;
     }
     integer(){
-
+        this._sequence.push('integer');
+        return this;
     }
     string(){
-
+        this._sequence.push('string');
+        return this;
     }
     required(){
-
+        this._sequence.push('required');
+        return this;
     }
     array(){
-
+        this._sequence.push('array');
+        return this;
+    }
+    arrayEach(){
+        this._sequence.push('arrayEach');
+        return this;
     }
     min(n){
-
+        this._sequence.push({min : n});
+        return this;
     }
     max(n){
-
+        this._sequence.push({max : n});
+        return this;
     }
-    unique(table, column){
-
+    unique(table, column, without = null){
+        this._sequence.push({unique : {table, column, without}});
+        return this;
+    }
+    file(){
+        this._sequence.push('file');
+        return this;
+    }
+    async validate(){
+        let key, val, err;
+        for(let i = 0; i < this._sequence.length; i++){
+            let seq = this._sequence[i];
+            if(seq && typeof seq === 'object' && !Array.isArray(seq)){//{...}
+                if('key' in seq){
+                    key  = seq['key'];
+                    val = this._body[key];
+                }else if(val !== undefined && 'unique' in seq){
+                    let exists = await DB(seq.unique.table).where(seq.unique.column, val).when(seq.unique.without, function (query) {
+                        query.where(seq.unique.column, '<>', seq.unique.without);
+                    }).exists();
+                    if(exists){
+                        err = 'The ' + key + ' with this value ' + val + ' already exists.';
+                        this._pushErr(key, err);
+                    }
+                }else if('min' in seq){
+                    if(val !== undefined && val < seq.min){
+                        err = 'The ' + key + ' value less then ' + seq.min + '.';
+                        this._pushErr(key, err);
+                    }
+                }else if('max' in seq){
+                    if(val !== undefined && val > seq.max){
+                        err = 'The ' + key + ' value greater then ' + seq.max + '.';
+                        this._pushErr(key, err);
+                    }
+                }
+            }else if(val !== undefined && seq === 'number'){
+                if(isNaN(val)){
+                    err = 'The ' + key + ' value is not a numeric.';
+                    this._pushErr(key, err);
+                }
+            }else if(val !== undefined && seq === 'integer'){
+                if(!Number.isInteger(parseFloat(val))){
+                    err = 'The ' + key + ' value is not a integer.';
+                    this._pushErr(key, err);
+                }
+            }else if(val !== undefined && seq === 'array'){
+                if(!Array.isArray(val)){
+                    err = 'The ' + key + ' is not an array.';
+                    this._pushErr(key, err);
+                }
+            }else if(seq === 'required'){
+                if(val === undefined){
+                    err = 'The ' + key + ' is required.';
+                    this._pushErr(key, err);
+                }
+            }
+        }
+        // this._errors = {a: ['qwerty']};
+        console.log(this._sequence);
+        console.log(this._errors);
+        return this._errors;
     }
 }
 
-async function V(req, res, validationErrors, dataObj) {
-    let types = [
-        'number', 'integer', 'float', 'positive', 'negative', 'boolean', 'string', 'array', 'json', 'required',
-        'file', 'min', 'max', 'unique', 'in', 'array_min', 'array_max', 'or', '', '', '', '',
-        '', '', '', '', '', '', '', '', '', '', '', ''
-    ];
 
-    let body = req.body ?? {};
-    let files = req.files ?? {};
-    return {
-        item(reqKey){
-
-        },
-
-    };
-
-
-
-
-}
-
-module.exports = {validate, api_validate, unique, ValidateClass, V};
+module.exports = {validate, api_validate, unique, VRequest};
--- http/console/commands/tmpCommand.js
+++ http/console/commands/tmpCommand.js
@@ -4,7 +4,7 @@ const moment = require("moment/moment");
 const {nodeCommand} = require("../kernel");
 const path = require('node:path');
 const TeamsResource = require('../../resources/teamsResource');
-const {ValidateClass, V} = require('../../../components/validate');
+const {VRequest} = require('../../../components/validate');
 class TmpCommand {
     constructor(args = []) {
         this.args = args;
@@ -12,6 +12,24 @@ class TmpCommand {
     static command = "tmp";
     async handle()
     {
+        let req = {
+            body: {
+                user_id: '1',
+                levels: '105.6',
+                gallery: ['qwerty', 'asd'],
+                nmb: '125.785'
+            },
+            files: null
+        }
+        let errors = await new VRequest(req, 'res')
+            .key('user_id').unique('users', 'id', 5).min(1).max(7)
+            .key('levels').integer().min(0).max(100).required()
+            .key('zmbrdm').required()
+            .key('gallery').array().arrayEach()
+            .key('nmb').number()
+            .validate()
+        ;
+        // console.log(errors);
 
 
 
